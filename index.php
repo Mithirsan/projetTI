a97a3a45d31635692e55f2713564511ad9e76ce5
Mithirsan <mithirsan@gmail.com>
2018-05-04 11:11:02 +0200

    index fonctionnel

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<HTML lang="en">
+    <HEAD>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="CSS/bootstrap.css" >
+        <script type="text/javascript" src="JS/jquery-3.3.1.js"></script>
+        <script type="text/javascript" src="JS/bootstrap.js"></script>
+        
+        
+        <!--OUR FILES -->
+<!--        <link rel="stylesheet" type="text/css" href="CSS/style.css" > -->
+        <script type="text/javascript" src="JS/script.js"></script>
+        
+        <!--FAVICON AND TITTLE -->
+        <link rel="icon" type="image/png" href="ressources/images/logo.png" />
+        <title>66</title>
+        
+    </HEAD>
+    <BODY>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-fixed-top">
+            <div class="container">
+                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#mainMenu" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon" />
+                </button>
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">66</a>
+                </div>
+                <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit"><span class="glyphicon glyphicon-search" ></span> Search</button>
+                </form>
+                <div class="navbar-collapse collapse" id="mainMenu">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="btn"><a href="#">Home</a></li>
+                        <li class="btn"><a href="#">Contact</a></li>
+                        <li class="btn"><a href="#">About</a></li>
+                        <li class="btn"><a href="#"><span class="glyphicon glyphicon-user"></span>  Sign up</a></li>
+                        <li class="btn"><a href="#"><span class="glyphicon glyphicon-log-in"></span> Log in</a></li>
+                    </ul>
+                </div>
+                
+            </div>
+        </nav>
+    
+    </BODY>
+</HTML>
