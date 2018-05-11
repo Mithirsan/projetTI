51c20e5ce12ce5f2127d7e4a34e1e3940be48375
Kevin Nobles <nobles-k@hotmail.com>
2018-05-11 11:42:24 +0200

    home

---
+++ home.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<HTML lang="en">
+    <HEAD>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="CSS/bootstrap.css" >
+        <script type="text/javascript" src="JS/jquery-3.3.1.js"></script>
+        <script type="text/javascript" src="JS/bootstrap.js"></script>
+        <script type="text/javascript" src="JS/script.js"></script>
+    </HEAD>
+    
+    <BODY>
+        <div id="homeCarousel" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#homeCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#homeCarousel" data-slide-to="1"></li>
+                <li data-target="#homeCarousel" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="..." alt="First slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="..." alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="..." alt="Third slide">
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#homeCarousel" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#homeCarousel" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </BODY>
+</HTML>
