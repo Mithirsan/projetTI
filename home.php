6ec903134b04c18b3a14c1e724e412cc3ddaeadb
Kevin Nobles <nobles-k@hotmail.com>
2018-05-11 12:44:18 +0200

    images

--- home.php
+++ home.php
@@ -4,27 +4,26 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" type="text/css" href="CSS/bootstrap.css" >
+        <link rel="stylesheet" type="text/css" href="CSS/style.css" >
         <script type="text/javascript" src="JS/jquery-3.3.1.js"></script>
         <script type="text/javascript" src="JS/bootstrap.js"></script>
         <script type="text/javascript" src="JS/script.js"></script>
     </HEAD>
     
     <BODY>
+        <header>
         <div id="homeCarousel" class="carousel slide" data-ride="carousel">
             <ol class="carousel-indicators">
                 <li data-target="#homeCarousel" data-slide-to="0" class="active"></li>
                 <li data-target="#homeCarousel" data-slide-to="1"></li>
                 <li data-target="#homeCarousel" data-slide-to="2"></li>
             </ol>
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img class="d-block w-100" src="..." alt="First slide">
+            <div class="carousel-inner embed-responsive embed-responsive-21by9">
+                <div class="carousel-item embed-responsive-item active " style="background-image:url('ressources/images/windwaker.png')">
                 </div>
-                <div class="carousel-item">
-                    <img class="d-block w-100" src="..." alt="Second slide">
+                <div class="carousel-item embed-responsive-item" style="background-image:url('ressources/images/majoras.png')">
                 </div>
-                <div class="carousel-item">
-                    <img class="d-block w-100" src="..." alt="Third slide">
+                <div class="carousel-item embed-responsive-item" style="background-image:url('ressources/images/afrosamurai.jpg')">
                 </div>
             </div>
             <a class="carousel-control-prev" href="#homeCarousel" role="button" data-slide="prev">
@@ -36,5 +35,6 @@
                 <span class="sr-only">Next</span>
             </a>
         </div>
+        </header>
     </BODY>
 </HTML>
