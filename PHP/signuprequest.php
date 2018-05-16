824c0985fc4e501d375f4013ae714f66a935f589
Tyrzal <la163284@student.helha.be>
2018-05-16 22:48:17 +0200

    signuprequest ok

--- PHP/signuprequest.php
+++ PHP/signuprequest.php
@@ -3,25 +3,25 @@
 	include "../admin.php";
 	
 	//Récupération des données du formulaire
-	$username = $_POST('username');
+	$username = $_POST['username'];
 //	$password = hash('sha256', $_POST('password'));		//cryptage du mot de passe
     $password = password_hash($_POST['pass'], PASSWORD_DEFAULT); //plus conforme actuellement.
-	$firstName = $_POST('firstName');
-	$lastName = $_POST('lastName');
-	$email = $_POST('email');
-	$birthDate = $_POST('date');
+	$firstName = $_POST['firstName'];
+	$lastName = $_POST['lastName'];
+	$email = $_POST['email'];
+	$birthDate = $_POST['date'];
 
-	if($_POST('phone')){
-		$phone = $_POST('phone');
+	if(isset($_POST['phone'])){
+		$phone = $_POST['phone'];
 		
 		//requête si phone
-		$query = 'INSERT INTO users(username, password, mail, birthDate, firstName, lastName, phone) VALUES ('.$username.', '.$password.', '.$mail.', '.$birthDate.', '.$firstName.', '.$lastName.', '.$phone.');';
+		$query = "INSERT INTO users(username, mail, password, birthDate, firstName, lastName, phone) VALUES ('$username','$mail','$password','$birthDate','$firstName','$lastName','$phone')";
 	}
 	
 	//requête sans phone
 	else
 	{
-		$query = 'INSERT INTO users(username, password, mail, birthDate, firstName, lastName) VALUES ('.$username.', '.$password.', '.$mail.', '.$birthDate.', '.$firstName.', '.$lastName.');';	
+		$query = "INSERT INTO users(username, mail, password, birthDate, firstName, lastName, phone) VALUES ('$username','$mail','$password','$birthDate','$firstName','$lastName')";
 	}
 	
 	//préparation de la requête
