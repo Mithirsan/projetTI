9215fd9be4816117a2bb390dc6057d5ce8ba9983
Kevin Nobles <nobles-k@hotmail.com>
2018-05-17 15:22:27 +0200

    fdsqfdsqf

--- navbar.php
+++ navbar.php
@@ -2,26 +2,7 @@
     session_start();
 ?>
 <HEAD>
-<<<<<<< HEAD
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" type="text/css" href="CSS/bootstrap.css" >
-        <script type="text/javascript" src="JS/jquery-3.3.1.js"></script>
-        <script type="text/javascript" src="JS/popper.js"></script>
-        <script type="text/javascript" src="JS/tooltip.js"></script>
-        <script type="text/javascript" src="JS/bootstrap.js"></script>
-        
-        
-        <!--OUR FILES -->
-       <link rel="stylesheet" type="text/css" href="CSS/style.css" >
-        <script type="text/javascript" src="JS/script.js"></script>
-        
-        <!--FAVICON AND TITTLE -->
-        <link rel="icon" type="image/png" href="ressources/images/logo.png" />
-        <title>HomeSweetHome</title>
-        
-    </HEAD>
-=======
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="CSS/bootstrap.css" >
@@ -29,7 +10,6 @@
     <script type="text/javascript" src="JS/popper.js"></script>
     <script type="text/javascript" src="JS/tooltip.js"></script>
     <script type="text/javascript" src="JS/bootstrap.js"></script>
->>>>>>> 6e439e02e0c0a4ba4cafec0e59a675544427fb70
 
 
     <!--OUR FILES -->
